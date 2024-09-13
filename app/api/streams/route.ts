6be9563a1d2fb8e9f6b6fb53268f098be20c3deb
rythm-sachdeva <rythmrko123@gmail.com>
2024-09-13 10:47:50 +0530

    Did Endpoint testing and debugging

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { prismaClient } from "../../lib/db";
 import { YT_REGEX } from "../../lib/utils";
 
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api"
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -24,13 +26,21 @@ export async function POST(req: NextRequest)
         }
 
         const extractedId = data.url.split("?v=")[1]; // returns an array of string before "?v=" and after;
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+        const Thumbnails = res.thumbnail.thumbnails;
+        Thumbnails.sort((a:{width:number},b:{width:number})=> a.width<b.width ? -1 : 1);
+        const vidtitle = res.title;
 
        const stream = await prismaClient.stream.create({
            data:{
             userId: data.creatorId,
             url: data.url,
             extractedId,
-            type: "Youtube"
+            type: "Youtube",
+            title: vidtitle ?? "Can't Find Video",
+            smalImg: Thumbnails.length>1 ? Thumbnails[Thumbnails.length -2].url : Thumbnails[Thumbnails.length-1].url ,
+            bigImg: Thumbnails[Thumbnails.length - 1].url ?? ""
+
            }
         })
 
